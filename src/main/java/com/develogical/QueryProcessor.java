f15455650ffbd54a3ca77aaf6b05bb460389b422
hredi <73148855+hredi@users.noreply.github.com>
2022-09-22 12:27:57 -0400

    should fail, ensuring only green builds deploy

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,10 +3,8 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+        if (query.toLowerCase().contains("Tell me a story")) {
+            return "I'm certain you've heard it before"
         }
         return "";
     }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -17,12 +17,12 @@ public class QueryProcessorTest {
 
     @Test
     public void knowsAboutShakespeare() throws Exception {
-        assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
+        assertThat(queryProcessor.process("Tell me a story"), containsString("heard"));
     }
 
     @Test
     public void isNotCaseSensitive() throws Exception {
-        assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
+        assertThat(queryProcessor.process("tell me a story"), containsString("heard"));
     }
 
 }
