ba02951f7da3e8cbe48ae04d55d833345dbd7e19
hredi <73148855+hredi@users.noreply.github.com>
2022-09-22 12:28:41 -0400

    query processor change to respond to tell me a story

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,7 +4,7 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.toLowerCase().contains("Tell me a story")) {
-            return "I'm certain you've heard it before"
+            return "I'm certain you've heard it before";
         }
         return "";
     }
