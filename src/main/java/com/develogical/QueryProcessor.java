f2f52889c90e1962ef81d1936ab25a8d0956295e
hredi <73148855+hredi@users.noreply.github.com>
2022-09-22 12:35:02 -0400

    fixed build error by changing query to lower case

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("Shakespeare")) {
+        if (query.toLowerCase().contains("shakespeare")) {
             return "He's a pretty cool guy";
         }
         return "";
